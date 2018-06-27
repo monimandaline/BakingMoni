a5a7e006a866f5f4c2b57cd4845c92dccbfdadef
Csontos Mónika <csm@mandaline.hu>
2018-06-27 17:13:06 +0200

    Initial commit

---
+++ app/src/main/java/hu/mandaline/bakingmoni/RecipeMainActivity.java
@@ -0,0 +1,13 @@
+package hu.mandaline.bakingmoni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_main);
+    }
+}
---
+++ app/src/main/java/hu/mandaline/bakingmoni/utils/RecipesApi.java
@@ -0,0 +1,12 @@
+package hu.mandaline.bakingmoni.utils;
+
+public class RecipesApi {
+
+   /* private RecipesApi() {
+
+    }*/
+
+    public static final class RecipesApiDbConstants {
+        public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+    }
+}
